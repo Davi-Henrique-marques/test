93315e1c35286b3c46500b2df997d87f06ef8b6d
Davi-Henrique-Marques <davihenriquemarques1002@gmail.com>
2026-09-24 15:56:06 -0300

    boolean e object

--- learnig/main.js
+++ learnig/main.js
@@ -3,18 +3,43 @@
  *   const -> constante, não posso trocar o valor da variavel
  *   var -> descontinuado
  * 
- *   consele.log() -> imprime no console 
+ *   console.log() -> imprime no console 
  *   "" -> string
  *   sem ""-> numero
  */
 
 let nome ="davi";
 let idade = 20;
-console.log(nome, idade);
 
 
 "texto com aspas duplas" /**texto de aspas simples/duplas são iguais */
 'texto com aspas simples'
-`texto 
-com 
-crase`/**com crase posso pular linha */
+`texto com crase` /**crase é usado para colocar variaveis dentro do texto */
+
+const mystring = `meu nome é ${nome} e minha idade é ${idade}`;
+console.log(mystring);
+
+
+/**
+ * number-numero
+ * string-texto
+ * boolean-verdadeiro ou falso
+ * object-objeto
+*/
+
+
+const name = "rodolfo";
+const age = 20;
+const adress = "rua dos bobos";
+
+const person = { /**o object serve para agrupar dados relacionados */
+    name: "rodolfo",
+    age: 20,
+    adress: { /** objeto dentro de outro objeto */
+        name: "davi",
+        idade: 15
+    }
+
+}
+
+console.log (person.adress)

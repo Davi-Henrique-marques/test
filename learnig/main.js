18747d456756db5ddb555a0a39573c10a63ed13c
Davi-Henrique-Marques <davihenriquemarques1002@gmail.com>
2026-09-24 16:31:24 -0300

    array

--- learnig/main.js
+++ learnig/main.js
@@ -43,3 +43,23 @@ const person = { /**o object serve para agrupar dados relacionados */
 }
 
 console.log (person.adress)
+
+
+/**
+ * array-É uma estrutura de dados que armazena uma coleção de elementos,
+ *       que podem ser de diferentes tipos, como números, strings, objetos, etc.
+ *       Os elementos em um array são indexados por números inteiros, começando do zero.
+ */
+
+const myArray = [1, "texto", true, { name: "davi", idade: 15 }, 5];/**utiliza-se colchetes no array e vírgula para separar os elementos */
+
+const gerenciamento = [ /**o array pode ser utilizado para guadar dados de todas as pessoas em um único lugar */
+    {name: "davi", idade: 15},
+    {name: "rodolfo", idade: 20},/** o object é utilizado para agrupar dados de certas pessoas */
+    {name: "joão", idade: 25}
+]
+
+/**               0  1  2  3   -- posição dos elementos começa no 0*/
+const myArray2 = [1, 2, 3, 4];
+
+console.log(myArray2[3]); /**para acessar um elemento do array, utiliza-se o nome do array e o índice do elemento entre colchetes */
